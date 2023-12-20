475aa121345fc0074ebb89f6316b6cb321f88604
arlenkamberi <arlenkamberi@gmail.com>
2023-12-20 19:40:06 +0100

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,10 +33,20 @@ class Base:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(list_dicts))
 
-if __name__ == "__main__":
-    r1 = Rectangle(10, 7, 2, 8)
-    r2 = Rectangle(2, 4)
-    Rectangle.save_to_file([r1, r2])
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation json_string """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
 
-    with open("Rectangle.json", "r") as file:
-        print(file.read())
+if __name__ == "__main__":
+    list_input = [
+        {'id': 89, 'width': 10, 'height': 4},
+        {'id': 7, 'width': 1, 'height': 7}
+    ]
+    json_list_input = Rectangle.to_json_string(list_input)
+    list_output = Rectangle.from_json_string(json_list_input)
+    print("[{}] {}".format(type(list_input), list_input))
+    print("[{}] {}".format(type(json_list_input), json_list_input))
+    print("[{}] {}".format(type(list_output), list_output))
